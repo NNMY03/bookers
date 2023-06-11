7196a0134a2b85746da433264be30821a7d1236b
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-06-11 12:52:22 +0000

    [Add]編集機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,34 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/index'
   end
 
   def index
-    @books = Book.all 
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def uddate
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to show_book(book.id)  
+  end
+
+ private
+ def book_params
+    params.require(:book).permit(:title, :body)
+ end
 end
