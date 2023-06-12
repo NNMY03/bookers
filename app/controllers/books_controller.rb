21a4e415a0d059ebd66f46aa96f3f4b862426517
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-06-12 08:32:32 +0000

    [Add]バリテーションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,22 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-   if  book.save
-    redirect_to books_path(@book.id)
+    @book = Book.new(book_params)
+   if  @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
    else
-    render :new
+     @books = Book.all
+    render :index
    end
   end
 
   def index
     @books = Book.all
-    
-    @book = Book.new 
+
+    @book = Book.new
   end
 
   def show
@@ -25,18 +27,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to show_book_path(book.id)  
+    redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
-  
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+
   end
 
  private
